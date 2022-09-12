2598502f7148f009a89e408469118307bc29550a
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-12 15:51:46 +0800

    update

--- www/NfcSessionAtOBJC.js
+++ www/NfcSessionAtOBJC.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.beginScan = function ( success, error) {
+exports.beginScan = function (success, error) {
     exec(success, error, 'NfcSessionAtOBJC', 'beginScan', []);
-};
-
-exports.tagReaderSession = function (arg0, success, error) {
-    exec(success, error, 'NfcSessionAtOBJC', 'tagReaderSession', [arg0]);
-};
-
-exports.tagReaderSession = function (arg0, success, error) {
-    exec(success, error, 'NfcSessionAtOBJC', 'tagReaderSession', [arg0]);
 };
