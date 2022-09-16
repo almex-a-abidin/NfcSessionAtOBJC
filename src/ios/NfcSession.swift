8ee2ea95ba31c576db96824c51caa31f6474ca1a
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-16 14:14:00 +0800

    Update NfcSession.swift

--- src/ios/NfcSession.swift
+++ src/ios/NfcSession.swift
@@ -119,11 +119,11 @@ import CoreNFC
     }
 }
 
-class TagData {
-    var uid: Data = Data()
-    var isLock: Bool?
-    var tagType: NFCTag?
-    var miFareFamily: NFCMiFareFamily = .unknown
-    var getVersion: Data = Data()
-    var recordLength: Int = -1
-}
+// class TagData {
+//     var uid: Data = Data()
+//     var isLock: Bool?
+//     var tagType: NFCTag?
+//     var miFareFamily: NFCMiFareFamily = .unknown
+//     var getVersion: Data = Data()
+//     var recordLength: Int = -1
+// }
