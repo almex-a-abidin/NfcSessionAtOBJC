ea0e1e871d11ba8218a720a108dd7f91e323e003
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-17 17:17:34 +0800

    Update NfcSession.swift

--- src/ios/NfcSession.swift
+++ src/ios/NfcSession.swift
@@ -41,8 +41,7 @@ import CoreNFC
         //     "name": "Art John Abidin",
         //     "age": "29"
         // ]
-         self.pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: tags);
-         self.commandDelegate!.send(self.pluginResult, callbackId: self.command!.callbackId);
+        print(tags) 
         if tags.count > 1 {
             // self.finishScan?(nil, "読み取りに失敗しました。再度お試しください。")
             self.pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "読み取りに失敗しました。再度お試しください。");
@@ -51,10 +50,10 @@ import CoreNFC
         }
         
         // タグがなかった場合
-        // guard let tag = tags.first else {
-        //     // self.finishScan?(nil, "読み取りに失敗しました。再度お試しください。")
-        //     self.session?.invalidate(errorMessage: "読み取りに失敗しました。再度お試しください。")
-        // }
+        guard let tag = tags.first else {
+            // self.finishScan?(nil, "読み取りに失敗しました。再度お試しください。")
+            self.session?.invalidate(errorMessage: "読み取りに失敗しました。再度お試しください。")
+        }
         
         // if case .miFare(let miFareTag) = tag {
         //     let tagData = TagData()
