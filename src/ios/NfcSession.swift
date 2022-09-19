359b343e00d4274dd5539a04a363f3aab3edbe41
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-19 09:44:41 +0800

    Update NfcSession.swift

--- src/ios/NfcSession.swift
+++ src/ios/NfcSession.swift
@@ -74,9 +74,9 @@ import CoreNFC
 
             var data = miFareTag.identifier as Data  
             let desiredString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-            print(desiredString)           
-            // self.pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: desiredString);
-            // self.commandDelegate!.send(self.pluginResult, callbackId: self.command!.callbackId);
+            let sample = desiredString as String           
+            self.pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: sample);
+            self.commandDelegate!.send(self.pluginResult, callbackId: self.command!.callbackId);
             // familly
             // var miFareFamily = miFareTag.mifareFamily. as String
             
