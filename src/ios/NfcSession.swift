015df34896504f48c1e7ad1ce2315dc68e6a86e8
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-20 08:36:45 +0800

    Update NfcSession.swift

--- src/ios/NfcSession.swift
+++ src/ios/NfcSession.swift
@@ -131,7 +131,7 @@ import CoreNFC
                             }
                
                             // var baseData = data.base64EncodedData()
-                            var backToString = String(data: data, encoding: String.Encoding.utf8) as String!
+                            var backToString = String(data: data, encoding: String.Encoding.utf8.rawValue) as String!
                             //self.nfcVersion = backToString
                             self.cdvCallbackSuccess()
                             self.session?.invalidate()
