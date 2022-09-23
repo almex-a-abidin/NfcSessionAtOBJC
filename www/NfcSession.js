3c426097d37f0a88a723a49d0eb82f217f5e8181
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-23 09:15:23 +0800

    Update NfcSession.js

--- www/NfcSession.js
+++ www/NfcSession.js
@@ -6,6 +6,10 @@ NfcSession.prototype.beginScan = function (success, error) {
     exec(success, error, 'NfcSession', 'beginScan', []);
 };
 
+NfcSession.prototype.getRecordedData = function (success, error) {
+    exec(success, error, 'NfcSession', 'getRecordedData', []);
+};
+
 
 if(!window.plugins)
     window.plugins = {};
